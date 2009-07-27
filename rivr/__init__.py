f42273071b1c4ed10de21f1441e94100804e7c7c
Kyle Fuller <inbox@kylefuller.co.uk>
2009-07-27 15:48:18 +0100

    Created a basic Request/Response

---
+++ rivr/__init__.py
@@ -0,0 +1,1 @@
+from rivr.http import Response
---
+++ rivr/http.py
@@ -0,0 +1,33 @@
+class Request(object):
+    def __init__(self, path='/', method='GET', GET={}, POST={}):
+        self.path = path
+        self.method = method
+        self.GET = GET
+        self.POST = POST
+
+class Response(object):
+    status_code = 200
+    
+    def __init__(self, content='', status=None, content_type='text/html; charset=utf8'):
+        self.content = content
+        
+        if status:
+            self.status_code = status
+        
+        self.headers = {'Content-Type': content_type}
+    
+    def __str__(self):
+        return '\n'.join(['%s: %s' % (key, value) for key, value in self.headers.items()]) + '\n\n' + self.content
+
+class ResponseRedirect(Response):
+    status_code = 302
+    
+    def __init__(self, redirect_to):
+        super(ResponseRedirect, self).__init__()
+        self.headers['Location'] = str(redirect_to)
+
+class ResponsePermanentRedirect(ResponseRedirect):
+    status_code = 301
+
+class ResponseNotFound(Response):
+    status_code = 404
