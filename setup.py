bb394287a36843b7ef632f0251f1f742ef7650aa
Kyle Fuller <inbox@kylefuller.co.uk>
2010-04-05 18:07:33 +0100

    Create a setup.py installer

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+VERSION = '0.1'
+
+setup(
+    name='rivr',
+    version=VERSION,
+    author='Kyle Fuller',
+    author_email='inbox@kylefuller.co.uk',
+    url='http://github.com/kylef/rivr/',
+    download_url='http://github.com/kylef/django-request/zipball/%s' % VERSION,
+    packages=['rivr', 'rivr.middleware', 'rivr.views'],
+    license='BSD',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
