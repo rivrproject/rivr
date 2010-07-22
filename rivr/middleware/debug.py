2ebe22aab10fda96d285b1d093b29247924ee127
Kyle Fuller <inbox@kylefuller.co.uk>
2010-07-22 22:35:09 +0100

    Use some CSS on the DebugMiddleware HTML page

--- rivr/middleware/debug.py
+++ rivr/middleware/debug.py
@@ -10,16 +10,33 @@ ERROR_HTML = """
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <title>Title</title>
+    <title>%(title)s</title>
+    <style type="text/css" media="screen">
+    html * { padding:0; margin:0; }
+    body * { padding:10px 20px; font-family: 'Helvetica Neue', HelveticaNeue, Arial, Helvetica, sans-serif; color: #555; }
+    body * * { padding:0; }
+    h1, h2 { font-weight: normal; margin-bottom:.4em; letter-spacing: -1px; }
+    h1 { font-size: 27px; }
+    h2 { font-size: 22px; }
+    #header { background: #eeeeee; border-bottom: 1px solid #ddd; }
+    #header h1 { text-shadow: #fff 2px 2px 0; }
+    #content { margin: 5px; }
+    pre { border: 1px solid #ddd; margin: 10px 0; background: #eee; width: 720px; padding: 10px; border-radius: 6px; overflow: auto; font-size: 9pt; font-weight: normal; }
+    code { font-family: Consolas, Monaco, 'Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier New'; }
+    </style>
 </head>
 <body>
-    <h1>%(title)s</h1>
+    <div id="header">
+        <h1>%(title)s</h1>
+    </div>
     
-    <h2>Traceback</h2>
-    <pre><code>%(traceback)s</code></pre>
-    
-    <h2>Request</h2>
-    <pre><code>%(request)s</code></pre>
+    <div id="content">
+        <h2>Traceback</h2>
+        <pre><code>%(traceback)s</code></pre>
+        
+        <h2>Request</h2>
+        <pre><code>%(request)s</code></pre>
+    </div>
 </body>
 """
 
