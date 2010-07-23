ca21919b3fb5a96c16387a70a883cc054728ead3
Kyle Fuller <inbox@kylefuller.co.uk>
2010-07-23 13:09:17 +0100

    Print the port rivr.serve is using

--- rivr/server.py
+++ rivr/server.py
@@ -4,4 +4,5 @@ from rivr.wsgi import WSGIHandler
 def serve(handler, host='localhost', port=8080):
     httpd = WSGIServer((host, port), WSGIRequestHandler)
     httpd.set_app(WSGIHandler(handler))
+    print "Server is running at http://%s:%s/" % (host, port)
     httpd.serve_forever()
