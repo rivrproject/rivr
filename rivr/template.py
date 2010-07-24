8594e7f11b78f7e74a3f87ba35c1d6bd15519f98
Kyle Fuller <inbox@kylefuller.co.uk>
2010-07-24 01:29:14 +0100

    Context can be option on render_to_string and render_to_response

--- rivr/template.py
+++ rivr/template.py
@@ -309,12 +309,12 @@ class Template(object):
             e.context = context
             raise e
 
-def render_to_string(template_string, context):
+def render_to_string(template_string, context={}):
     t = Template(template_string)
     c = Context(context)
     return t.render(c)
 
-def render_to_response(template_string, context, *args, **kwargs):
+def render_to_response(template_string, context={}, *args, **kwargs):
     return Response(render_to_string(template_string, context), *args, **kwargs)
 
 def html_escape(text):
