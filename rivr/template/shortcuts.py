014b2b000e46c37d8c033c6c18ffc97329f7bd14
Kyle Fuller <inbox@kylefuller.co.uk>
2010-07-28 00:28:21 +0100

    Change the render_to_response shortcuts to use template files, and update the example_template.py

--- rivr/template/shortcuts.py
+++ rivr/template/shortcuts.py
@@ -1,10 +1,20 @@
 from rivr.http import Response
 from rivr.template import Template, Context
+from rivr.template.loader import Loader
 
-def render_to_string(template_string, context={}):
-    t = Template(template_string)
+def render_to_string(template, template_dirs=None, context={}):
+    l = Loader()
+    
+    if template_dirs:
+        if isinstance(template_dirs, list):
+            l.template_dirs += template_dirs
+        else:
+            l.template_dirs.append(template_dirs)
+    
+    t = l.get_template(template)
     c = Context(context)
+    c['loader'] = l
     return t.render(c)
 
-def render_to_response(template_string, context={}, *args, **kwargs):
-    return Response(render_to_string(template_string, context), *args, **kwargs)
+def render_to_response(template, template_dirs=None, context={}, *args, **kwargs):
+    return Response(render_to_string(template, template_dirs, context), *args, **kwargs)
