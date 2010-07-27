e3df46b2481111cc8632eb7d319b806229c94531
Kyle Fuller <inbox@kylefuller.co.uk>
2010-07-27 18:31:39 +0100

    Add templatefilters to get the first and last element in an list

--- rivr/template/defaultfilters.py
+++ rivr/template/defaultfilters.py
@@ -5,3 +5,17 @@ register = template.Library()
 def escape(value):
     return template.html_escape(value)
 register.filter('escape', escape)
+
+def first(value):
+    try:
+        return value[0]
+    except IndexError:
+        return ''
+register.filter('first', first)
+
+def last(value):
+    try:
+        return value[-1]
+    except IndexError:
+        return ''
+register.filter('last', last)
