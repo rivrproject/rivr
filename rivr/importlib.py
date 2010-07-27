bf8ba9c85c3ce342806bad6291197c9104ea4681
Kyle Fuller <inbox@kylefuller.co.uk>
2010-07-27 16:52:33 +0100

    Fix a bug in importlib not importing some modules

--- rivr/importlib.py
+++ rivr/importlib.py
@@ -5,7 +5,7 @@ def import_module(name):
     if not isinstance(name, str):
         raise ImportError, 'rivr.import.import_module cannot import %s because it is not a string.' % name
     
-    module = __import__('.'.join(name.split('.')[:-1]))
+    module = __import__(name)
     for i in name.split('.')[1:]:
         module = getattr(module, i)
     
