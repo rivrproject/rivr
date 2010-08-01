afcb100e712123f31f2c2761b8832054d5641594
Kyle Fuller <inbox@kylefuller.co.uk>
2010-08-01 17:50:36 +0100

    Update the router example to include regex groups

--- example_router.py
+++ example_router.py
@@ -6,8 +6,12 @@ def index(request):
 def test(request):
     return rivr.Response('test view.')
 
+def page(request, slug):
+    return rivr.Response('page view, slug is `%s`' % slug)
+
 if __name__ == '__main__':
     rivr.serve(rivr.Router(
         (r'^$', index),
-        (r'^test/$', test)
+        (r'^test/$', test),
+        (r'^(?P<slug>[-\w]+)/$', page)
     ))
