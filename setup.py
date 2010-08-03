a219c09ad621fb5ffd7e612b7e6d186bb5f03a8d
Kyle Fuller <inbox@kylefuller.co.uk>
2010-08-03 17:17:35 +0100

    Update setup.py and include rivr.templates

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ setup(
     author_email='inbox@kylefuller.co.uk',
     url='http://github.com/kylef/rivr/',
     download_url='http://github.com/kylef/rivr/zipball/%s' % VERSION,
-    packages=['rivr', 'rivr.middleware', 'rivr.views'],
+    packages=['rivr', 'rivr.middleware', 'rivr.views', 'rivr.template'],
     license='BSD',
+    summary='rivr is a microweb framework inspired by djng',
+    description="rivr is a microweb framework inspired by djng, the reason I decided to create rivr and not use djng was that djng still depended on Django. I wanted rivr for places where I don't have Django. It is a lightweight framework which can be included along side another python application. rivr does not have a database layer, you are free to use whatever you choose.",
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
