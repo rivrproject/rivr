96a0578a8cce6396e7436b8fcbeec8f9e444d08b
Kyle Fuller <inbox@kylefuller.co.uk>
2010-10-14 03:10:02 +0100

    Add a example of using rivr with the zokket WSGIServer

--- rivr/views/__init__.py
+++ rivr/views/__init__.py
@@ -1,4 +1,6 @@
-from rivr.http import ResponseRedirect
+from rivr.http import ResponseRedirect, ResponsePermanentRedirect
 
-def redirect_to(request, url):
-    return ResponseRedirect(url)
+def redirect_to(request, url, permanent=False, **kwargs):
+    if permanent:
+        return ResponsePermanentRedirect(url % kwargs)
+    return ResponseRedirect(url % kwargs)
