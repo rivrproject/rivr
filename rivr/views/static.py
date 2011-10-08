d9f9d4460358d1304501707a67cca43c3f69579f
Kyle Fuller <inbox@kylefuller.co.uk>
2011-10-08 11:42:49 +0000

    Turn the static views into a class-based view

--- rivr/views/static.py
+++ rivr/views/static.py
@@ -6,77 +6,10 @@ import stat
 import re
 from email.Utils import formatdate, parsedate_tz, mktime_tz
 
+from rivr.views.base import View
 from rivr.http import Response, ResponseRedirect, ResponseNotModified, Http404
+from rivr.template import Context, Template
 
-def was_modified_since(header=None, mtime=0, size=0):
-    if header is None:
-        return True
-    
-    matches = re.match(r'^([^;]+)(; length=([0-9]+))?$', header, re.IGNORECASE)
-    
-    header_mtime = mktime_tz(parsedate_tz(matches.group(1)))
-    header_len = matches.group(3)
-    
-    if header_len and int(header_len) != size:
-        return True
-    
-    if mtime > header_mtime:
-        return True
-    
-    return False
-
-def serve(request, path, document_root=None, show_indexes=False):
-    """
-    Usage in the router:
-    
-    (r'(?P<path>.*)$', 'rivr.views.serve', {'document_root':'/usr/var/http/'})
-    """
-    
-    # Clean up given path to only allow serving files below document_root.
-    path = posixpath.normpath(urllib.unquote(path))
-    path = path.lstrip('/')
-    newpath = ''
-    
-    for part in path.split('/'):
-        if not part:
-            # Strip empty path components.
-            continue
-        
-        drive, part = os.path.splitdrive(part)
-        head, part = os.path.split(part)
-        
-        if part in (os.curdir, os.pardir):
-            # Strip '.' and '..' in path.
-            continue
-        
-        newpath = os.path.join(newpath, part).replace('\\', '/')
-    
-    if newpath and path != newpath:
-        return ResponseRedirect(newpath)
-    
-    fullpath = os.path.join(document_root, newpath)
-    
-    if not os.path.exists(fullpath):
-        raise Http404('"%s" does not exist.' % newpath)
-    
-    if os.path.isdir(fullpath):
-        if show_indexes:
-            return directory_index(request, newpath, fullpath)
-        raise Http404('Directory indexes are not allowed here.')
-    
-    statobj = os.stat(fullpath)
-    
-    if not was_modified_since(request.META.get('HTTP_IF_MODIFIED_SINCE'), statobj[stat.ST_MTIME], statobj[stat.ST_SIZE]):
-        return ResponseNotModified()
-    
-    mimetype = mimetypes.guess_type(fullpath)[0] or 'application/octet-stream'
-    contents = open(fullpath, 'rb').read()
-    
-    response = Response(contents, content_type=mimetype)
-    response.headers['Last-Modified'] = '%s GMT' % formatdate(statobj[stat.ST_MTIME])[:25]
-    response.headers['Content-Length'] = str(len(contents))
-    
-    return response
 
 DEFAULT_DIRECTORY_INDEX_TEMPLATE = """
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
@@ -95,7 +28,7 @@ DEFAULT_DIRECTORY_INDEX_TEMPLATE = """
       {% ifnotequal directory "/" %}
         <li><a href="../">../</a></li>
       {% endifnotequal %}
-      
+
       {% for f in files %}
         <li><a href="{{ f }}">{{ f }}</a></li>
       {% endfor %}
@@ -103,19 +36,98 @@ DEFAULT_DIRECTORY_INDEX_TEMPLATE = """
 </body>
 """
 
-def directory_index(request, path, fullpath):
-    from django.template import Context, Template
+class StaticView(View):
+    """
+    Usage in the router:
+
+        (r'(?P<path>.*)$', StaticView.as_view(document_root='.', show_indexes=True))
+    """
+
+    document_root = None
+    show_indexes = False
+    use_request_path = False
+
+    def was_modified_since(self, mtime=0, size=0):
+        header = self.request.META.get('HTTP_IF_MODIFIED_SINCE')
+        if header is None:
+            return True
+
+        matches = re.match(r'^([^;]+)(; length=([0-9]+))?$', header, re.IGNORECASE)
+
+        header_mtime = mktime_tz(parsedate_tz(matches.group(1)))
+        header_len = matches.group(3)
+
+        if header_len and int(header_len) != size:
+            return True
+
+        if mtime > header_mtime:
+            return True
+
+        return False
+
+    def directory_index(self, request, path, fullpath):
+        if not self.show_indexes:
+            raise Http404('Directory indexes are not allowed here.')
+
+        t = Template(DEFAULT_DIRECTORY_INDEX_TEMPLATE)
+
+        files = []
+        for f in os.listdir(fullpath):
+            if not f.startswith('.'):
+                if os.path.isdir(os.path.join(fullpath, f)):
+                    f += '/'
+                files.append(f)
+
+        return Response(str(t.render(Context({
+            'directory': path + '/',
+            'files': files,
+        }))))
+
+    def get(self, request, path=''):
+        if self.use_request_path:
+            path = request.path
+
+        # Clean up given path to only allow serving files below document_root.
+        path = posixpath.normpath(urllib.unquote(path))
+        path = path.lstrip('/')
+        newpath = ''
+
+        for part in path.split('/'):
+            if not part:
+                # Strip empty path components.
+                continue
+
+            drive, part = os.path.splitdrive(part)
+            head, part = os.path.split(part)
+
+            if part in (os.curdir, os.pardir):
+                # Strip '.' and '..' in path.
+                continue
+
+            newpath = os.path.join(newpath, part).replace('\\', '/')
+
+        if newpath and path != newpath:
+            return ResponseRedirect(newpath)
+
+        fullpath = os.path.join(self.document_root, newpath)
     
-    t = Template(DEFAULT_DIRECTORY_INDEX_TEMPLATE)
-    
-    files = []
-    for f in os.listdir(fullpath):
-        if not f.startswith('.'):
-            if os.path.isdir(os.path.join(fullpath, f)):
-                f += '/'
-            files.append(f)
-    
-    return Response(str(t.render(Context({
-        'directory': path + '/',
-        'files': files,
-    }))))
+        if not os.path.exists(fullpath):
+            raise Http404('"%s" does not exist.' % newpath)
+
+        if os.path.isdir(fullpath):
+            return self.directory_index(request, newpath, fullpath)
+
+        statobj = os.stat(fullpath)
+
+        if not self.was_modified_since(statobj[stat.ST_MTIME], statobj[stat.ST_SIZE]):
+            return ResponseNotModified()
+
+        mimetype = mimetypes.guess_type(fullpath)[0] or 'application/octet-stream'
+        contents = open(fullpath, 'rb').read()
+
+        response = Response(contents, content_type=mimetype)
+        response.headers['Last-Modified'] = '%s GMT' % formatdate(statobj[stat.ST_MTIME])[:25]
+        response.headers['Content-Length'] = str(len(contents))
+
+        return response
+
