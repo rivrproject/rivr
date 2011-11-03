37d947d2c7d7f07d011b49e1177fe159330de9a4
Kyle Fuller <inbox@kylefuller.co.uk>
2011-11-03 13:40:10 +0000

    Add a example_sessions which shows using a sessions store

---
+++ example_session.py
@@ -0,0 +1,42 @@
+"""
+Example rivr.sessions using a in-memory session store which is
+not persistent across restarts. See rivr.mongodb.sessions for
+persistent sessions.
+"""
+
+import rivr
+from rivr.sessions import *
+
+class SessionView(rivr.views.TemplateView):
+    template_name = "sessions.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs['session_key'] = self.request.session.session_key
+        kwargs['session'] = self.request.session.data
+        return kwargs
+
+    def post(self, request, *args, **kwargs):
+        request.session['name'] = request.POST.get('name')
+        return self.get(request, *args, **kwargs)
+
+class ClearView(rivr.views.RedirectView):
+    url = '/'
+    permanent = False
+
+    def get(self, request, *args, **kwargs):
+        request.session.clear()
+        return super(ClearView, self).get(request, *args, **kwargs)
+
+router = rivr.Router(
+    (r'^clear/$', ClearView.as_view()),
+    (r'^$', SessionView.as_view())
+)
+
+app = rivr.MiddlewareController(router,
+    SessionMiddleware(session_store=MemorySessionStore()),
+    rivr.TemplateMiddleware('example_templates/')
+)
+
+if __name__ == '__main__':
+    rivr.serve(app)
+
