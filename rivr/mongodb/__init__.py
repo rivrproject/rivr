42130a82f68e54f7b7bbc6105027effcfd7c9cde
Kyle Fuller <inbox@kylefuller.co.uk>
2011-11-03 09:18:04 +0000

    Refactor MongoDB database from URI out of the middleware

--- rivr/mongodb/__init__.py
+++ rivr/mongodb/__init__.py
@@ -1,9 +1,24 @@
 import os
+import pymongo
 from urlparse import urlparse
 
 from rivr.middleware import Middleware
 
-import pymongo
+def get_database(uri=None):
+    if not uri and 'MONGODB_URI' in os.environ:
+        uri = os.environ['MONGODB_URI']
+
+    if not uri:
+        raise Exception("MongoDB URI unconfigured")
+
+    m = urlparse(uri)
+    connection = pymongo.Connection(m.hostname, m.port)
+    database = connection[m.path.strip('/')]
+
+    if m.username and m.password:
+        database.authenticate(m.username, m.password)
+
+    return database
 
 class MongoMiddleware(Middleware):
     database = None
@@ -11,19 +26,7 @@ class MongoMiddleware(Middleware):
 
     def get_database(self):
         if not self.database:
-            if not self.uri and 'MONGODB_URI' in os.environ:
-                self.uri = os.environ['MONGODB_URI']
-
-            if not self.uri:
-                raise Exception("MongoMiddleware improperly configured")
-
-            m = urlparse(self.uri)
-            self.connection = pymongo.Connection(m.hostname, m.port)
-
-            self.database = self.connection[m.path.strip('/')]
-            
-            if m.username and m.password:
-                self.database.authenticate(m.username, m.password)
+            self.database = get_database(self.uri)
 
         return self.database
 
