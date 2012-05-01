ea04d8e395db4bd176ee8cf7e5f483cd6b8c72a3
Kyle Fuller <inbox@kylefuller.co.uk>
2012-05-01 23:01:09 +0100

    Change middleware so that we always handle a response even if we returned a response from our handle_request

--- rivr/middleware/base.py
+++ rivr/middleware/base.py
@@ -6,22 +6,19 @@ class Middleware(object):
             setattr(self, key, value)
 
     def __call__(self, request):
+        response = None
+
         if hasattr(self, 'process_request'):
             response = self.process_request(request)
 
-            if response:
-                return response
-
-        try:
-            response = self.handler(request)
-        except Exception, e:
-            if hasattr(self, 'process_exception'):
-                response = self.process_exception(request, e)
-
-                if response:
-                    return response
-            else:
-                raise e
+        if not response:
+            try:
+                response = self.handler(request)
+            except Exception, e:
+                if hasattr(self, 'process_exception'):
+                    response = self.process_exception(request, e)
+                else:
+                    raise e
 
         if hasattr(self, 'process_response'):
             response = self.process_response(request, response)
