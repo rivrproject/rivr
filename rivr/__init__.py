d838ce2aca18167263a10d31edd274a837f76496
Kyle Fuller <inbox@kylefuller.co.uk>
2012-05-10 01:37:34 +0100

    Remove RedirectMiddleware and the Array dispatcher

--- rivr/__init__.py
+++ rivr/__init__.py
@@ -6,10 +6,8 @@ from rivr.server import serve
 from rivr.middleware import Middleware, MiddlewareController, ErrorWrapper
 from rivr.middleware.debug import DebugMiddleware
 from rivr.middleware.auth import AuthMiddleware
-from rivr.middleware.redirect import RedirectMiddleware
 
 from rivr.router import Router, Domain, url, include
-from rivr.array import Array
 
 from rivr.template import Template, Context
 from rivr.template.response import TemplateResponse, TemplateMiddleware, direct_to_template
