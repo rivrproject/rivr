92117ee48195137f801776633434258778a4cc48
Kyle Fuller <inbox@kylefuller.co.uk>
2012-05-11 11:55:55 +0100

    Fix the example middleware

--- example_middleware.py
+++ example_middleware.py
@@ -8,7 +8,7 @@ def view(request):
     raise Exception, "Debugger example"
 
 if __name__ == '__main__':
-    rivr.serve(rivr.MiddlewareController(view,
+    rivr.serve(rivr.MiddlewareController.wrap(view,
         ExampleMiddleware(),  # This example middleware will run instead of our view on process_request
         rivr.DebugMiddleware(),
     ))
