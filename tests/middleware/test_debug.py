ff925331c2f4d1b1aa3add32a96521919216045a
Kyle Fuller <inbox@kylefuller.co.uk>
2015-01-04 01:23:57 +0000

    [Tests] Debug Middleware

---
+++ tests/middleware/test_debug.py
@@ -0,0 +1,32 @@
+import unittest
+from rivr.middleware.debug import DebugMiddleware
+from rivr.http import Http404
+
+
+class DebugMiddlewareTests(unittest.TestCase):
+    def test_catches_exception(self):
+        def view(request):
+            raise Exception('Testing')
+
+        middleware = DebugMiddleware.wrap(view)
+        response = middleware(None)
+
+        self.assertEqual(response.status_code, 500)
+
+    def test_errors_when_nothing_is_returned_from_view(self):
+        def view(request):
+            pass
+
+        middleware = DebugMiddleware.wrap(view)
+        response = middleware(None)
+
+        self.assertEqual(response.status_code, 500)
+
+    def test_catches_Http404(self):
+        def view(request):
+            raise Http404('Page not found')
+
+        middleware = DebugMiddleware.wrap(view)
+        response = middleware(None)
+
+        self.assertEqual(response.status_code, 404)
