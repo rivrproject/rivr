07c87b05af3ee0039692904c9896c7d13f426f3f
Kyle Fuller <kyle@fuller.li>
2020-10-07 20:20:09 +0100

    feat: add typing for TestClient path

--- rivr/tests.py
+++ rivr/tests.py
@@ -24,17 +24,17 @@ class TestClient(object):
 
         return self.handler(request)
 
-    def get(self, *args, **kwargs) -> Response:
-        return self.http('GET', *args, **kwargs)
+    def get(self, path: str, *args, **kwargs) -> Response:
+        return self.http('GET', path, *args, **kwargs)
 
-    def put(self, *args, **kwargs) -> Response:
-        return self.http('PUT', *args, **kwargs)
+    def put(self, path: str, *args, **kwargs) -> Response:
+        return self.http('PUT', path, *args, **kwargs)
 
-    def post(self, *args, **kwargs) -> Response:
-        return self.http('POST', *args, **kwargs)
+    def post(self, path: str, *args, **kwargs) -> Response:
+        return self.http('POST', path, *args, **kwargs)
 
-    def delete(self, *args, **kwargs) -> Response:
-        return self.http('DELETE', *args, **kwargs)
+    def delete(self, path: str, *args, **kwargs) -> Response:
+        return self.http('DELETE', path, *args, **kwargs)
 
 
 TestClient.__test__ = False  # type: ignore
