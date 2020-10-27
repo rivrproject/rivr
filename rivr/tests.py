0c1dd57581ef32d8c0838b759a50f312b87093e0
Kyle Fuller <kyle@fuller.li>
2020-10-27 01:59:28 +0000

    feat: add typing for TestClient

--- rivr/tests.py
+++ rivr/tests.py
@@ -20,26 +20,66 @@ class TestClient(object):
         request = Request(path, method, headers=headers, query=query, body=body)
         return self.handler(request)
 
-    def options(self, path: str, *args, **kwargs) -> Response:
-        return self.http('OPTIONS', path, *args, **kwargs)
+    def options(
+        self,
+        path: str,
+        query: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+        body: Optional[Union[bytes, IO[bytes]]] = None,
+    ) -> Response:
+        return self.http('OPTIONS', path, query=query, headers=headers, body=body)
 
-    def head(self, path: str, *args, **kwargs) -> Response:
-        return self.http('HEAD', path, **kwargs)
+    def head(
+        self,
+        path: str,
+        query: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+    ) -> Response:
+        return self.http('HEAD', path, query=query, headers=headers)
 
-    def get(self, path: str, *args, **kwargs) -> Response:
-        return self.http('GET', path, *args, **kwargs)
+    def get(
+        self,
+        path: str,
+        query: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+    ) -> Response:
+        return self.http('GET', path, query=query, headers=headers)
 
-    def put(self, path: str, *args, **kwargs) -> Response:
-        return self.http('PUT', path, *args, **kwargs)
+    def put(
+        self,
+        path: str,
+        query: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+        body: Optional[Union[bytes, IO[bytes]]] = None,
+    ) -> Response:
+        return self.http('PUT', path, query=query, headers=headers, body=body)
 
-    def post(self, path: str, *args, **kwargs) -> Response:
-        return self.http('POST', path, *args, **kwargs)
+    def post(
+        self,
+        path: str,
+        query: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+        body: Optional[Union[bytes, IO[bytes]]] = None,
+    ) -> Response:
+        return self.http('POST', path, query=query, headers=headers, body=body)
 
-    def patch(self, path: str, *args, **kwargs) -> Response:
-        return self.http('PATCH', path, *args, **kwargs)
+    def patch(
+        self,
+        path: str,
+        query: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+        body: Optional[Union[bytes, IO[bytes]]] = None,
+    ) -> Response:
+        return self.http('PATCH', path, query=query, headers=headers, body=body)
 
-    def delete(self, path: str, *args, **kwargs) -> Response:
-        return self.http('DELETE', path, *args, **kwargs)
+    def delete(
+        self,
+        path: str,
+        query: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+        body: Optional[Union[bytes, IO[bytes]]] = None,
+    ) -> Response:
+        return self.http('DELETE', path, query=query, headers=headers, body=body)
 
 
 TestClient.__test__ = False  # type: ignore
