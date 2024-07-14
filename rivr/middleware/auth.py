2230ca4a224eec3e15d328cc757a0007f5eba894
Kyle Fuller <kyle@fuller.li>
2024-07-14 12:37:52 +0100

    refactor(auth): method per auth scheme

--- rivr/middleware/auth.py
+++ rivr/middleware/auth.py
@@ -38,7 +38,7 @@ class AuthMiddleware(Middleware):
     needs_auth = True
 
     def process_request(self, request: Request) -> Optional[Response]:
-        user = self.check_login(request)
+        user = self.authorize(request)
         setattr(request, 'user', user)
 
         if not user.is_authenticated() and self.needs_auth:
@@ -46,24 +46,30 @@ class AuthMiddleware(Middleware):
 
         return None
 
-    def check_login(self, request: Request) -> Union[AnnonymousUser, User]:
+    def authorize(self, request: Request) -> Union[AnnonymousUser, User]:
         authorization = request.authorization
         if not authorization:
             return AnnonymousUser()
 
         scheme, token = authorization
-        if scheme.casefold() != 'basic':
-            return AnnonymousUser()
-
-        try:
-            username, password = b64decode(token).decode('utf-8').split(':')
-        except ValueError:
-            return AnnonymousUser()
-
-        if self.check_password(username, password):
-            return User(username)
+        if scheme.casefold() == 'basic':
+            return self.authorize_basic_token(request, token)
 
         return AnnonymousUser()
 
-    def check_password(self, username: str, password: str) -> bool:
+    def authorize_basic_token(self, request: Request, token: str) -> Union[AnnonymousUser, User]:
+        try:
+            userid, password = b64decode(token).decode('utf-8').split(':')
+        except ValueError:
+            return AnnonymousUser()
+
+        return self.authorize_basic(request, userid, password)
+
+    def authorize_basic(self, request: Request, userid: str, password: str) -> Union[AnnonymousUser, User]:
+        if self.check_password(userid, password):
+            return User(userid)
+
+        return AnnonymousUser()
+
+    def check_password(self, userid: str, password: str) -> bool:
         return False
